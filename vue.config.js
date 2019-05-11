e7622b6bf440a495002e5a8cf64181649a54b002
dumptyd <dumptyd2.0@gmail.com>
2019-05-12 02:01:57 +0530

    Upgrade packages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   productionSourceMap: false,
-  baseUrl: process.env.NODE_ENV === 'production' ? '/vue-css-donut-chart/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-css-donut-chart/' : '/'
 };
